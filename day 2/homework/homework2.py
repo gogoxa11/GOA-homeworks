7939cd5d8caeed7b246341bf9a5440184124c7c6
gogoxa11 <lukagogokhia345@gmail.com>
2024-02-20 21:16:30 +0400

    commit

--- day 2/homework/homework2.py
+++ day 2/homework/homework2.py
@@ -4,7 +4,7 @@ third = "Goa doesn't sponsor stupid companies and fake influencers for money!"
 fourth = "Goa makes your dreams come true like creating a platform etc..."
 fifth = "For now it is the best programming school in  Georgia and i'm confident that soon it will be the best programming school in the world."
 sixth = "It just takes some time."
-
+i_met = "I only met my group leader Giorgi Vanishvili yet"
 
 
 
